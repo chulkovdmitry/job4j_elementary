2a6b3ec334eb1f1853cf09d563d50def716f1dce
chulkovdmitry <dmitry_c2000@mail.ru>
2021-03-17 15:31:10 +0300

    6. Идеальный вес [#499825]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,13 +2,13 @@ package ru.job4j.converter;
 
 public class Converter {
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+        // int rsl = value / 70;
+        return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
+        // int rsl = value / 60; /* формула перевода рублей в доллары. */
+        return value / 60;
     }
 
     public static void main(String[] args) {
