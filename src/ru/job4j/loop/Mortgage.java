4782923d7cd3168d4764ce124145eb241e890bc2
chulkovdmitry <dmitry_c2000@mail.ru>
2021-03-24 12:35:50 +0300

    5.7. Ипотека [#499884]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        while (amount >= 0) {
+        while (amount > 0) {
             amount = amount - salary + (int) (amount * percent / 100);
             year++;
         }
