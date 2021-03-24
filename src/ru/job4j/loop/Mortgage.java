a2348aecb6c184a2cc8ea9b48cc7edf707346d21
chulkovdmitry <dmitry_c2000@mail.ru>
2021-03-24 09:51:05 +0300

    5.7. Ипотека [#499884]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+            amount = amount - salary + (int) (amount * percent / 100);
+            year++;
+        }
+        return year;
+    }
+}
